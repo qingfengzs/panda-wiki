c206e6e7a3928f2df5412d09d64410c68db0d699
yu.jin <yu.jin@bizatmobile.com>
2021-04-23 16:57:33 +0800

    Initial commit

---
+++ src/main/java/com/softium/panda/pandawiki/PandaWikiApplication.java
@@ -0,0 +1,13 @@
+package com.softium.panda.pandawiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandaWikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softium/panda/pandawiki/PandaWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softium.panda.pandawiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandaWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
